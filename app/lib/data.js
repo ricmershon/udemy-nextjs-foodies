8f12f944de8f26f28c7da28cc6896ad6ed3f501d
Ric Mershon <rmersh@ricmershon.com>
2024-10-09 21:40:25 -0400

    Meals error page

--- app/lib/data.js
+++ app/lib/data.js
@@ -3,7 +3,8 @@ import sql from 'better-sqlite3';
 const db = sql('meals.db');
 
 export async function fetchMeals() {
-    // 2 second simulated delay
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    // 3 second simulated delay
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // throw new Error('Loading meals failed');
     return db.prepare('SELECT * FROM meals').all();
 }
